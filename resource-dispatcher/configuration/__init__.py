ea7836851ec7090f67ceee26a13738d615c90cb2
Jacob See <jacob.d.see@gmail.com>
2021-07-07 21:18:06 -0700

    Add warning if trying to load single non-yaml config file, differentiate single file & directory config init message, notify when ignoring files in the directory, and warn when a config file has extraneous parameters.

--- resource-dispatcher/configuration/__init__.py
+++ resource-dispatcher/configuration/__init__.py
@@ -14,13 +14,17 @@ def get_config():
     template_vars = {"ENV_" + var: os.environ[var] for var in os.environ}
 
     if mode == "file":
+        if not os.environ["CONFIG_FILE"].endswith(".yml") and not os.environ["CONFIG_FILE"].endswith(".yaml"):
+            print("Configuration file must be in *.yml or *.yaml format.")
+            raise Exception("Configuration file not in required format")
         f = open(os.environ["CONFIG_FILE"], "r")
         contents = f.read()
         f.close()
         contents_template = Template(contents)
         templated_contents = contents_template.render(**template_vars)
         configuration = yaml.load(templated_contents, Loader=yaml.FullLoader)
-        print(f"Loaded configuration file: {f.name}")
+        print(f"Loaded single configuration file: {f.name}")
+        config_warnings(os.environ["CONFIG_FILE"], configuration)
         return configuration
 
     if mode == "dir":
@@ -28,6 +32,7 @@ def get_config():
             "repositories": [],
             "tasks": []
         }
+        print(f"Looking for configuration files in directory: {os.environ['CONFIG_DIR']}")
         for file in os.listdir(os.environ["CONFIG_DIR"]):
             if file.endswith(".yml") or file.endswith(".yaml"):
                 path = os.path.join(os.environ["CONFIG_DIR"], file)
@@ -41,5 +46,17 @@ def get_config():
                     configuration["repositories"].extend(config_tmp["repositories"])
                 if "tasks" in config_tmp:
                     configuration["tasks"].extend(config_tmp["tasks"])
-                print(f"Loaded configuration file: {path}")
+                print(f"Found configuration file: {path}")
+                config_warnings(path, config_tmp)
+            else:
+                print(f"Skipping file (not *.yml or *.yaml): {os.path.join(os.environ['CONFIG_DIR'], file)}")
         return configuration
+
+
+def config_warnings(filename, config):
+    keys_defined = set(config.keys())
+    keys_understood = {"repositories", "tasks"}
+    extra_keys = keys_defined.difference(keys_understood)
+    if len(extra_keys):
+        print(f"Warning: Fields defined in configuration file but not used: {extra_keys}")
+    pass
